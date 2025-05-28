96b64ce6256b2053d5349b89e796e349a42b92a1
najakorn <90379306+najakorn@users.noreply.github.com>
2025-05-28 15:15:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,21 +44,52 @@ reward_gifs = [
     },
     {
         "gif": "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjhnaTJjOGdpZ21mOXpnajd3cHpueHNyNHVwZHl0eTR5aHExbDk5eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DorxfW5xBGSG8bVxRa/giphy.gif",
-        "caption": "That’s 5 more days of cuteness. Virtual hug loading..."
+        "caption": "Virtual hug loading..."
     },
     {
         "gif": "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbDFkc3N3anBkM29ocW5hYXV2ODVhMmxxeDg4dnVzOHd5ODdmb2h3byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/T0ph30SEe8W15fLHIU/giphy.gif",
-        "caption": "OMG 5 more days?? You’re on a roll and I’m OBSESSED"
+        "caption": "MISS YOU"
     },
     {
         "gif": "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcm9paWdhYmg3dzg3NGd1emYzZTB5cXlhYmk3ems4ODN1dzlnNHFkNyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Bv76prPEWdr4nxovaK/giphy.gif",
-        "caption": "Another 5-day streak! You’ve earned the right to kiss me and play with me like this!"
+        "caption": "This could be you and me"
     },
     {
         "gif": "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXp1dWNhc3dvMG80enJmdXB6Ymo4ejBtYWxrcWV3NGtmOTRiamU1eiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/H4uE6w9G1uK4M/giphy.gif",
-        "caption": "Another 5-day streak! Wait for meee!! I’m waddling as fast as I can to come kiss you!"
+        "caption": "Wait for meee!! I’m waddling as fast as I can to come kiss you!"
     },
-    
+    {
+        "gif": "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTFiM3VpbnluOG5jamc2Z3p6aDN2enVtNDdpNmhjamxncW1ob2N2ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/04T0w4STSpBu0s3LEo/giphy.gif",
+        "caption": "Me when I see your morning texts"
+    },
+    {
+        "gif": "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjJqMGhxa281eTA3Zmp1bG12YjFidDB1eW5nYXIxMWE5c2NkemRyeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/v13QrrN1StLd6/giphy.gif",
+        "caption": "You and me walking into any room ready to take over the world!"
+    },
+    {
+        "gif": "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTQ3cmJ1em5pbXRseGltaHY4bTF1OTV0c2NnM202ZTE4MW0yOHI3ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UdGsqvzhHRD7QtC0VA/giphy.gif",
+        "caption": "Probably one of Genie's magic"
+    },
+    {
+        "gif": "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTQ3cmJ1em5pbXRseGltaHY4bTF1OTV0c2NnM202ZTE4MW0yOHI3ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UdGsqvzhHRD7QtC0VA/giphy.gif",
+        "caption": "Us time soon please!"
+    },
+    {
+        "gif": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWwydzdjanl5OW5nYm84dzRtZnRzYmwwdG1laGh0eDI0c3ZiaHhuciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/12XDYvMJNcmLgQ/giphy.gif",
+        "caption": "JIAYOU!!"
+    },
+    {
+        "gif": "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTZobHJwMG1jZTcyOXg5NDAzMHQ5aHg4d2Jmd2w1N3B6Y2dpYTNocyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/bGyKPJ40CDJja/giphy.gif",
+        "caption": "You're still not here.."
+    },
+    {
+        "gif": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2UwaWxwMnR3Z2phMHpnaGtnNG5xZGZyMW9pbjdsdmdxdHFodzZoZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/tQAApm4PMOpiM/giphy.gif",
+        "caption": "Miss you otterly"
+    },
+    {
+        "gif": "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExemkxd3N1bHlycW42aXRhMzJsMDd1cG9pOHkyNTM4cmJuOHV0dnNnaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xOvhMKk3EyS1LKQtkK/giphy.gif",
+        "caption": "Kiss attackkk"
+    }
 ]
 
 genie_note = st.text_area("Tell me about it:", placeholder="")
@@ -94,7 +125,11 @@ if st.button("Submit"):
         else:
             st.session_state.streak = 1
         st.session_state.last_checkin = today
-
+    
+    # **just for today**
+    st.session_state.streak = 5
+    # ******************
+    
     final_message = genie_note.strip() if genie_note.strip() else random.choice(messages[score])
 
     st.markdown(
