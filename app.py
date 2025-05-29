2732128dd08d94c90665b4dca6bbda2c894bee86
najakorn <90379306+najakorn@users.noreply.github.com>
2025-05-29 10:08:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,10 +82,6 @@ Message: {final_message}"""
 
     # if st.button("💌 New message for you"):
     #     st.markdown("")
-        
-    # **just for today**
-    st.session_state.streak = 3
-    # ******************
     
     if st.session_state.streak % 3 == 0 and st.session_state.streak > 0:
         reward = random.choice(reward_gifs)
