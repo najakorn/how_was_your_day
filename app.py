03083713fcf181d336a12abbf0df6b98ca90a869
najakorn <90379306+najakorn@users.noreply.github.com>
2025-05-30 17:20:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -107,8 +107,7 @@ if st.session_state.show_love_letter:
         ">
         Happy Saturday, baobeir 🌞<br><br>
     
-        Hope your friend’s 40th birthday is a fun one.  
-        Cheers to good people and good moments!<br><br>
+        Hope your friend’s birthday is a fun one. Cheers to good people and good moments!<br><br>
     
         Just wanted to say thank you again for being open during our call these past couple of nights,  
         even when the conversation got real.  
