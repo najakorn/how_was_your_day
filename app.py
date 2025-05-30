f9f13fc5bbcc0e2f47822c8082f5a63b4dce78c2
najakorn <90379306+najakorn@users.noreply.github.com>
2025-05-30 17:03:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ if "show_love_letter" not in st.session_state:
     st.session_state.show_love_letter = False
 
 # Button sets the flag to True
-if st.button("New message for you"):
+if st.button("New message for you 📬📝"):
     st.session_state.show_love_letter = True
 
 # Only show the message if the flag is True
